53574c66a5ec220f236bd8f7940ac0a78166c739
Chris Jones <chris@cjmobileapps.com>
2024-10-02 00:26:34 -0500

    Respond with 200

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,6 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
 import java.net.ServerSocket;
 
 fun main() {
@@ -5,12 +8,30 @@ fun main() {
     println("Logs from your program will appear here!")
 
     // Uncomment this block to pass the first stage
-     var serverSocket = ServerSocket(4221)
+    var serverSocket = ServerSocket(4221)
 
-     // Since the tester restarts your program quite often, setting SO_REUSEADDR
-     // ensures that we don't run into 'Address already in use' errors
-     serverSocket.reuseAddress = true
+    // Since the tester restarts your program quite often, setting SO_REUSEADDR
+    // ensures that we don't run into 'Address already in use' errors
+    serverSocket.reuseAddress = true
 
-     serverSocket.accept() // Wait for connection from client.
-     println("accepted new connection")
+    // To try this locally on macOS, you could run ./your_server.sh in one terminal session,
+    // and nc -vz 127.0.0.1 4221 in another. (-v gives more verbose output, -z just scan for listening daemons, without sending any data to them.)
+
+    val clientSocket = serverSocket.accept() // Wait for connection from client.
+    println("accepted new connection")
+
+    //while (true) {
+
+    val input = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
+    val output = PrintWriter(clientSocket.getOutputStream(), true)
+
+
+    // Read the request line
+    val requestLine = input.readLine()
+    println("Request: $requestLine")
+
+    output.println("HTTP/1.1 200 OK\\r\\n\\r\\n")
+    //output.println("Content-Type: text/plain");
+    //}
+
 }
