8cbdd2d078c7a002cd43d3d4f1d9babf98ba113d
Chris Jones <chris@cjmobileapps.com>
2024-10-03 02:07:21 -0500

    Extract URL path

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -16,18 +16,85 @@ fun main() {
 
     // To try this locally on macOS, you could run ./your_server.sh in one terminal session,
     // and nc -vz 127.0.0.1 4221 in another. (-v gives more verbose output, -z just scan for listening daemons, without sending any data to them.)
+    // curl -v http://localhost:4221
 
-    val clientSocket = serverSocket.accept() // Wait for connection from client.
-    println("accepted new connection")
-    val input = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
-    val output = PrintWriter(clientSocket.getOutputStream(), true)
+    while (true) {
+        val clientSocket = serverSocket.accept() // Wait for connection from client.
+        println("accepted new connection")
+        val input = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
+        val output = PrintWriter(clientSocket.getOutputStream(), true)
 
-    // Read the request line
-    val requestLine = input.readLine()
-    println("Request: $requestLine")
-    val httpResponse = "HTTP/1.1 200 OK\r\n\r\n"
-    println("httpResponse $httpResponse")
+        // Read the request line
+        val requestStatusLine = input.readLine()
+        println("requestStatusLine: $requestStatusLine")
 
-    output.print(httpResponse)
-    output.close()
+        val requestHostPort = input.readLine()
+        println("requestUrl: $requestHostPort")
+
+        val requestUserAgent = input.readLine()
+        println("requestUserAgent: $requestUserAgent")
+
+        val requestHeader = input.readLine()
+        println("requestHeader: $requestHeader")
+
+        // I think this is request body
+        val requestBody = input.readLine()
+        println("requestBody: $requestBody")
+
+        val httpResponse = buildResponse(
+            requestStatusLine = requestStatusLine,
+            requestHostPort = requestHostPort,
+        )
+        println()
+        println("httpResponse $httpResponse")
+
+        output.print(httpResponse)
+        output.close()
+    }
+    //output.close()
+}
+
+fun buildResponse(requestStatusLine: String, requestHostPort: String): String {
+
+    // Status
+    val responseStatusLine = buildResponseStatusLine(requestStatusLine, requestHostPort)
+
+    // Headers (Empty)
+    val crlfHeadersLine = "\r\n"
+
+    // Response body (empty)
+
+    return "$responseStatusLine $crlfHeadersLine"
+}
+
+fun buildResponseStatusLine(
+    requestStatusLine: String,
+    requestHostPort: String,
+): String {
+    // GET /index.html HTTP/1.1
+    val requestStatusLineArray = requestStatusLine.split(" ")
+
+    // Host: localhost:4221\r\n
+    val requestUrlArray = requestHostPort.split(" ")
+
+    // localhost:4221  /index.html
+    val requestUrl = requestUrlArray[1] + requestStatusLineArray[1]
+    val localServerUrl = "localhost:4221/"
+
+    val httpVersion = "HTTP/1.1"
+    val crlfStatusLine = "\r\n"
+
+    return if (requestUrl == localServerUrl) {
+        val statusCode = "200"
+        val optionalReasonPhrase = "OK"
+
+        // Status line
+        "$httpVersion $statusCode $optionalReasonPhrase$crlfStatusLine"
+    } else {
+        val statusCode = "404"
+        val optionalReasonPhrase = "Not Found"
+
+        // Status line
+        "$httpVersion $statusCode $optionalReasonPhrase$crlfStatusLine"
+    }
 }
