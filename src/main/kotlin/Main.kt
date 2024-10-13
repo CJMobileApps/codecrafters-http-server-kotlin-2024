fb32dfc7f2ca6bb0e3f9e1560edc2b6432cd9967
Chris Jones <chris@cjmobileapps.com>
2024-10-12 19:21:36 -0500

    Read request body : Part 3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -100,6 +100,7 @@ fun ServerResponse.buildResponseStatusLine(
         }
 
         if (requestStatusLineArray[1] == "/user-agent") {
+            this.contentType = "Content-Type: text/plain\r\n"
             this.content = serverRequest.getUserAgent()
             return this.setFoundOk()
         }
